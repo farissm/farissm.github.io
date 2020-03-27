0d81fd146f4b2ce0cfcff732910be0215535cf40
farissm <farismuhammad2198@gmail.com>
2020-03-27 12:55:13 +0700

    masih newbie

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # farissm.github.io
+
+## Bill splitter
+this app is useful to split your bill when you hangout with your friends, family, etc.
+
+## how to use
+just input the total costs, number of person, the taxes, and you will know how much money you should pay 
---
+++ index.js
@@ -0,0 +1,27 @@
+const split = () =>{
+    let total = parseInt(document.getElementById('total').value)
+    let person = parseInt(document.getElementById('person').value)
+    let taxes = document.getElementById('taxs')
+
+    if(taxes.value === ""){
+        const result = total/person
+        document.getElementById('costPerson').innerHTML = result.toFixed()
+    } else {
+        let tax = parseInt(taxes.value)
+        const result = (total + ((tax * total)/100))/person
+        document.getElementById('costPerson').innerHTML = result.toFixed()
+    }
+}
+
+const sptBtn = document.getElementById('splitbtn')
+sptBtn.addEventListener('click', (ev) =>{
+    ev.preventDefault()
+    split()
+})
+
+const rstBtn = document.getElementById('resetbtn')
+rstBtn.addEventListener('click', (ev) =>{
+    // ev.preventDefault()
+    document.getElementsByClassName('frm').reset()
+    document.getElementById('costPerson').innerHTML = '0'
+})
